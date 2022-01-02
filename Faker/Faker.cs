ad5054836150be45e214ce128ba52237426ab8a5
Anna <makarushko7702@gmail.com>
2022-01-02 15:40:12 +0300

    initial

---
+++ Faker/Faker.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Faker
+{
+    class Faker
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
