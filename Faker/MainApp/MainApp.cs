5d5c0e02aaaa731ef0f806385fed67562d068eb7
Anna <makarushko7702@gmail.com>
2022-01-25 23:16:39 +0300

    add int generator

---
+++ Faker/MainApp/MainApp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Xml;
+using Faker.DTO;
+using Newtonsoft.Json;
+
+namespace Faker.MainApp
+{
+    internal static class MainApp
+    {
+        private static void Main(string[] args)
+        {
+
+            Faker faker = new Faker();
+
+            var user = faker.Create<User>();
+            Console.WriteLine(JsonConvert.SerializeObject(user, Formatting.Indented));
+
+        }
+    }
+}
